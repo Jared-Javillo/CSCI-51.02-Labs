8c4cfd013b108c75883440755551e6785235a6ca
965anton <antonmmcastillo@gmail.com>
2021-05-24 23:20:38 +0800

    Added boolean to arrange by burst time and other lines of code

--- Project 1/sjf.h
+++ Project 1/sjf.h
@@ -2,16 +2,24 @@
 
 using namespace std;
 
-// bool compBurstTime(const vector<int> &a,const vector<int> &b)
-// {
-//     return 
-// }
+bool compBurstTime(const vector<int> &a,const vector<int> &b)
+{
+    return a[1] < b[1];
+}
 
 void doProcessSJF(vector<vector<int>> processVec, int numProcess )
 {
     int output = 0;
     int timePassed = 0;
     sort(processVec.begin(), processVec.end());
+    sort(processVec.begin()+1, processVec.end(), compBurstTime);
+    int tempCompletionTime = processVec[0][0];
+    cout << tempCompletionTime <<" "<< processVec[0][3] <<" "<< processVec[0][1]<<"x"<<"\n";
+    for (int i = 1; i < numProcess; i++)
+    {
+        tempCompletionTime = tempCompletionTime + processVec[i-1][1];
+        cout << tempCompletionTime <<" "<< processVec[i][3] <<" "<< processVec[i][1]<<"x"<<"\n";
+    }
     for (int i = 0; i < numProcess; i++)
     {
         if(timePassed < processVec[i][0])
@@ -21,8 +29,12 @@ void doProcessSJF(vector<vector<int>> processVec, int numProcess )
         {
             timePassed +=  processVec[i][1];
         }
-        cout << processVec[i][0] <<" "<< processVec[i][3] <<" "<< processVec[i][1]<<"x"<<"\n";
+        output += processVec[i][1];
     }
-
-
+    cout <<"Total time elapsed: " << timePassed <<"ns\n";
+    cout <<"Total CPU burst time: " << output <<"ns"<< "\n";
+    output = ( static_cast<float>(output) / static_cast<float>(timePassed) ) *100;
+    cout <<"CPU Utilization: "<< output << "%\n";
+    cout << "Throughput: " << static_cast<float>(numProcess) / static_cast<float>(timePassed) << " processes/ns" <<"\n";
+    
 }
