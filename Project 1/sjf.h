77332d3d3ae34f00f0ca906b95c960eeb1abc1ae
965anton <antonmmcastillo@gmail.com>
2021-05-24 20:26:31 +0800

    edited SJF and compiled main.cpp to test

--- Project 1/sjf.h
+++ Project 1/sjf.h
@@ -2,12 +2,16 @@
 
 using namespace std;
 
+// bool compBurstTime(const vector<int> &a,const vector<int> &b)
+// {
+//     return 
+// }
+
 void doProcessSJF(vector<vector<int>> processVec, int numProcess )
 {
     int output = 0;
     int timePassed = 0;
-
-    sort(processVec.begin(), processVec.end(),compArrTime);
+    sort(processVec.begin(), processVec.end());
     for (int i = 0; i < numProcess; i++)
     {
         if(timePassed < processVec[i][0])
@@ -19,4 +23,6 @@ void doProcessSJF(vector<vector<int>> processVec, int numProcess )
         }
         cout << processVec[i][0] <<" "<< processVec[i][3] <<" "<< processVec[i][1]<<"x"<<"\n";
     }
+
+
 }
