a0d995c9377692287eed7f322b77deb97a9a9491
965anton <antonmmcastillo@gmail.com>
2021-05-25 19:46:01 +0800

    Added void method for P

---
+++ Project 1/p.h
@@ -0,0 +1,10 @@
+#include <vector>
+
+using namespace std;
+
+void doProcessP(vector<vector<int>> processVec, int numProcess)
+{
+    //arranged by arrival time
+    sort(processVec.begin(), processVec.end());
+    
+}
