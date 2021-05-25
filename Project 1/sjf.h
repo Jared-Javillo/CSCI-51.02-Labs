e5d1584e910bbe49f98292dfd0df1a8ae02f5941
965anton <antonmmcastillo@gmail.com>
2021-05-25 21:18:28 +0800

    Finished SJF AGAIN

--- Project 1/sjf.h
+++ Project 1/sjf.h
@@ -2,10 +2,6 @@
 
 using namespace std;
 
-bool compProcessNumber(const vector<int> &a,const vector<int> &b)
-{
-    return a[3] < b[3];
-}
 bool compBurstTime(const vector<int> &a,const vector<int> &b)
 {
     return a[1] < b[1];
@@ -87,60 +83,22 @@ void doProcessSJF(vector<vector<int>> processVec, int numProcess )
         //Add waiting time for process in arrivedVec
         for (int i = 0; i < arrivedVec.size(); i++)
         {
-            waitingTimes[arrivedVec[0][3]-1] ++;
+            waitingTimes[arrivedVec[i][3]-1] ++;
         }
-
-        
-
         worldTime++;
         
     }
-    // sort(processVec.begin()+1, processVec.end(), compBurstTime);
-    // //to help get Start time of each process
-    // int tempStartTime = processVec[0][0];
-    // vector<vector<int>> tempSTVec;
-    // vector<int> initiaL;
-    // initiaL.push_back(tempStartTime);
-    // initiaL.push_back(processVec[0][3]);
-    // tempSTVec.push_back(initiaL);
-    // cout << tempStartTime <<" "<< processVec[0][3] <<" "<< processVec[0][1]<<"x"<<"\n";
-    // for (int i = 1; i < numProcess; i++)
-    // {
-    //     vector<int> tempHolder;
-    //     tempStartTime = tempStartTime + processVec[i-1][1];
-    //     cout << tempStartTime <<" "<< processVec[i][3] <<" "<< processVec[i][1]<<"x"<<"\n";
-    //     tempHolder.push_back(tempStartTime);
-    //     tempHolder.push_back(processVec[i][3]);
-    //     tempSTVec.push_back(tempHolder);
-    // }
-    // //to get total elapsed time of CPU
-    // for (int i = 0; i < numProcess; i++)
-    // {
-    //     if(timePassed < processVec[i][0])
-    //     {
-    //         timePassed += (processVec[i][0] - timePassed) + processVec[i][1];
-    //     }else if(timePassed >= processVec[i][0])
-    //     {
-    //         timePassed +=  processVec[i][1];
-    //     }
-    //     cPUBurst += processVec[i][1];
-    // }
-
-
+   
     cout <<"Total time elapsed: " << worldTime <<"ns\n";
     cout <<"Total CPU burst time: " << cPUBurst <<"ns"<< "\n";
     cPUBurst = ( static_cast<float>(cPUBurst) / static_cast<float>(worldTime) ) *100;
     cout <<"CPU Utilization: "<< cPUBurst << "%\n";
     cout << "Throughput: " << static_cast<float>(numProcessConst) / static_cast<float>(worldTime) << " processes/ns" <<"\n";
-    // sort(processVec.begin(), processVec.end(), compProcessNumber);
-    // sort(tempSTVec.begin(), tempSTVec.end(), compBurstTime);
-
+    
     //Waiting time
     cout << "Waiting Times: \n";
     for (int i = 0; i < numProcessConst; i++)
     {
-        //cout << " Process " << i+1 << ": " << tempSTVec[i][0] - processVec[i][0] << "ns\n";
-        //total = total + (tempSTVec[i][0] - processVec[i][0]);
         cout << " Process " << i + 1 << ": " << waitingTimes[i] << "ns\n";
         total += waitingTimes[i];
     }
@@ -150,8 +108,6 @@ void doProcessSJF(vector<vector<int>> processVec, int numProcess )
     cout << "Turnaround Times: \n";
     for (int i = 0; i < numProcessConst; i++)
     {
-        // cout << " Process " << i+1 << ": " << processVec[i][1]+(tempSTVec[i][0] - processVec[i][0]) << "ns\n";
-        // total = total + (processVec[i][1]+(tempSTVec[i][0] - processVec[i][0]));
         cout << " Process " << i + 1 << ": " << waitingTimes[i] + burstTimes[i]<< "ns\n";
         total += waitingTimes[i] + burstTimes[i];
     }
@@ -161,8 +117,6 @@ void doProcessSJF(vector<vector<int>> processVec, int numProcess )
     cout << "Response Times: \n";
     for (int i = 0; i < numProcessConst; i++)
     {
-        // cout << " Process " << i+1 << ": " << tempSTVec[i][0] - processVec[i][0] << "ns\n";
-        // total = total + (tempSTVec[i][0] - processVec[i][0]);
         cout << " Process " << i + 1 << ": " << startTimes[i] - arrivalTimes[i]<< "ns\n";
         total +=  startTimes[i] - arrivalTimes[i];
     }
