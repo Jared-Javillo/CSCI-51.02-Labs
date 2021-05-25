0551e9b0045a577e8358e2b9495e98296603e769
Terariver <jared.javillo@obf.ateneo.edu>
2021-05-25 14:24:40 +0800

    added stats per process

--- Project 1/srtf.h
+++ Project 1/srtf.h
@@ -12,14 +12,26 @@ void doProcessSRTF(vector<vector<int>> processVec, int numProcess)
     int finProcess = 0;
     int worldTime = 0;
     int procTime = 0;
+    int total = 0;
+
     vector<vector<int>> arrivedVec;
     vector<int> solving;
+    vector<int> waitingTimes;
+    vector<int> burstTimes;
+    vector<int> arrivalTimes;
+    vector<int> startTimes;
 
     int totalCPU = 0;
     const int numProcessConst = numProcess;
+
+    //Filling Vectors for Output
     for (int i = 0; i < numProcess; i++)
     {
         totalCPU += processVec[i][1];
+        burstTimes.push_back(processVec[i][1]);
+        waitingTimes.push_back(0);
+        startTimes.push_back(0);
+        arrivalTimes.push_back(processVec[i][0]);
     }
 
     sort(processVec.begin(), processVec.end(), compArrTime);
@@ -44,12 +56,15 @@ void doProcessSRTF(vector<vector<int>> processVec, int numProcess)
         //Solve Processes
         sort(arrivedVec.begin(), arrivedVec.end(), compBurstProcNum);
 
+        //If no process in CPU
         if (solving.empty())
         {
+            //add first process to CPU
             solving = arrivedVec[0];
             arrivedVec.erase(arrivedVec.begin());
         }
 
+        //if ArrivedVec process is faster than process CPU (Premption)
         if (arrivedVec[0][1] < solving[1])
         {
             cout << worldTime - procTime<< " " << solving[3] << " " << procTime << "\n";
@@ -59,18 +74,36 @@ void doProcessSRTF(vector<vector<int>> processVec, int numProcess)
             procTime = 0;
         }
 
+        //If process in CPU
         if (!solving.empty())
         {
+            //if its the process's first time
+            if (burstTimes[solving[3] - 1] == solving[1])
+            {
+                startTimes[solving[3] - 1] = worldTime;
+            }
+            
+            //Process is processing
             solving[1] -= 1;
             procTime ++;
+            //If process is finished
             if (solving[1] <= 0)
             {
+                //delete process from CPU and output
                 solving[1] = 0;
                 cout << worldTime - procTime +1 << " " << solving[3] << " " << procTime << "x\n";
                 solving.clear();
                 procTime = 0;
             }
         }
+
+        //Add waiting time for process in arrivedVec
+        for (int i = 0; i < arrivedVec.size(); i++)
+        {
+            waitingTimes[arrivedVec[0][3]-1] ++;
+        }
+
+        
 
         worldTime++;
     }
@@ -81,4 +114,32 @@ void doProcessSRTF(vector<vector<int>> processVec, int numProcess)
     cout << "Throughput: " << static_cast<float>(numProcessConst) / static_cast<float>(worldTime);
     cout << " processes/ns" <<"\n";
 
+    //Waiting Times
+    cout << "Waiting Times: \n";
+    for (int i = 0; i < numProcessConst; i++)
+    {
+        cout << " Process " << i + 1 << ": " << waitingTimes[i] << "ns\n";
+        total += waitingTimes[i];
+    }
+    cout << "Average waiting time: " << static_cast<float>(total) /static_cast<float>(numProcessConst) << "ns\n";
+    
+    //Turnaround Times
+    cout << "Turnaround Times: \n";
+    total = 0;
+    for (int i = 0; i < numProcessConst; i++)
+    {
+        cout << " Process " << i + 1 << ": " << waitingTimes[i] + burstTimes[i]<< "ns\n";
+        total += waitingTimes[i] + burstTimes[i];
+    }
+    cout << "Average turnaround time: " << static_cast<float>(total) /static_cast<float>(numProcessConst) << "ns\n";
+
+    //Response Times
+    cout << "Response Times: \n";
+    total = 0;
+    for (int i = 0; i < numProcessConst; i++)
+    {
+        cout << " Process " << i + 1 << ": " << startTimes[i] - arrivalTimes[i]<< "ns\n";
+        total +=  startTimes[i] - arrivalTimes[i];
+    }
+    cout << "Average Response time: " << static_cast<float>(total) /static_cast<float>(numProcessConst) << "ns\n";
 }
